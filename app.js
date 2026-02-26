7b00d5c265ea15eb4495c7c456cfd672cd22df51
Deepak kumar V <deepakvenu777@gmail.com>
2026-02-26 10:24:44 +0530

    Added Dockerfile

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     res.writeHead(200);
-    res.end("DevOps CI/CD Project Deployed Successfully!");
+    res.end("Server is working properly!");
 });
 
-server.listen(3000);
+server.listen(3000, () => {
+    console.log("Server started on port 3000");
+});
